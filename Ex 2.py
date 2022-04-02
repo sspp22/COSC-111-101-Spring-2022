8ccfc8cf3a6972ab4c85ff6698119603fd7ae089
sokpanha.c <100827096+sspp22@users.noreply.github.com>
2022-04-02 22:38:14 +0700

    header
    same as ex 1

--- Ex 2.py
+++ Ex 2.py
@@ -1,4 +1,4 @@
-
+print("You are given an array with positive numbers and a number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.")
 def index_power(array,index):
     try:
         array[index]    
